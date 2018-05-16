8bb023d50ffed2602db868723c95009cf79ec8d3
kevinjoseph1995 <kevjoseph@umass.edu>
2018-05-16 00:15:46 -0400

    04/16/18

--- main.py
+++ main.py
@@ -16,32 +16,58 @@ class variational_autoencoder(tf.keras.Model):
         self.upconv4=tf.keras.layers.Conv2DTranspose(filters=1,kernel_size=3,strides=2,padding='same',data_format='channels_last',dilation_rate=1,activation='relu',use_bias=True)
     def call(self, inputs, training=False):
         #input shape (samples, rows, cols, channels)
-        print (inputs.shape)
         l1=self.conv1(inputs)
-        print (l1.shape)
+        #print (l1.shape)
         l2=self.conv2(l1)
-        print (l2.shape)
+        #print (l2.shape)
         l3=self.conv3(l2)
-        print (l3.shape)
+        #print (l3.shape)
         l4=self.conv4(l3)
-        print (l4.shape)
+        #print (l4.shape)
         l5=self.upconv1(l4)
-        print (l5.shape)
+        #print (l5.shape)
         l6=self.upconv2(l5)
-        print (l6.shape)
+        #print (l6.shape)
         l7=self.upconv3(l6)
-        print (l7.shape)
+        #print (l7.shape)
         output=self.upconv4(l7)
-        print (output.shape)
+        #print (output.shape)
         #output shape (samples, new_rows, new_cols, filters)
         return output
 model=variational_autoencoder()
 
+def loss(model,input):
+    output=model(input)
+    input=tf.squeeze(input)
+    output=tf.squeeze(output)
+    return tf.reduce_mean(tf.reduce_sum(tf.square(input-output),axis=[1,2]))
+
+def grad(model, input  ):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, input)
+  return tape.gradient(loss_value, model.variables)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+
 
 training_data=dataset.train('./datasets')
-training_data=training_data.shuffle(60000).repeat(4).batch(32)
-iterator = training_data.make_one_shot_iterator()
-next_element = iterator.get_next()
-input=tf.reshape(next_element[0],[next_element[0].shape[0],28,28,1])
+training_data=training_data.shuffle(60000).repeat(10).batch(32)
 
-output=model(input)
+iterator = training_data.make_one_shot_iterator()
+
+next_element,_ = iterator.get_next()
+input=tf.reshape(next_element,[next_element.shape[0],28,28,1])#(samples, rows, cols, channels)
+print(loss(model, input).shape)
+
+print("Initial loss: {:.3f}".format(loss(model, input)))
+
+for (i, (next_element, _ )) in enumerate(training_data):
+    input=tf.reshape(next_element,[next_element.shape[0],28,28,1])#(samples, rows, cols, channels)
+    # Calculate derivatives of the input function with respect to its parameters.
+    grads = grad(model,input)
+    # Apply the gradient to the model
+    optimizer.apply_gradients(zip(grads, model.variables),global_step=tf.train.get_or_create_global_step())
+    if i % 200 == 0:
+        print("Loss at step {:04d}: {:.3f}".format(i, loss(model, input)))
+
+print("Final loss: {:.3f}".format(loss(model, input)))
